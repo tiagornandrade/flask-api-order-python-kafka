3560875da067c8cca640e0dc1809df4f9e941e6f
Tiago Navarro <tiagornandrade@hotmail.com>
2023-01-14 12:15:09 -0300

    feat: minimal application

---
+++ transaction.py
@@ -0,0 +1,22 @@
+import json
+
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
+
+ORDER_KAFKA_TOPIC = "order_details"
+ORDER_CONFIRMED_KAFKA_TOPIC = "order_confirmed"
+
+consumer = KafkaConsumer(
+    ORDER_KAFKA_TOPIC,
+    bootstrap_servers='localhost:9092'
+)
+producer = KafkaProducer(
+    bootstrap_servers='localhost:9092'
+)
+
+print("Gonna start listening..")
+while True:
+    for message in consumer:
+        print("Ongoing transaction..")
+        consumed_message = json.loads(message.value.decode())
+        print(consumed_message)
