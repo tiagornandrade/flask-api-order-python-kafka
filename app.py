2c153ac86bed411c16645009d523df7c6eacd9b4
DEV-BOOK\tiago <tiagornandrade@hotmail.com>
2023-01-16 09:18:12 -0300

    feat(web): add new features

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, request, jsonify 
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return {"message": "Hello World"}
+@app.route("/", methods=["GET","POST"])
+def create_event():
+    return jsonify(request.json)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
