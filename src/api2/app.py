14519c27ea570720046c52d456c8cae05d1122bd
Tiago Navarro <tiagornandrade@hotmail.com>
2023-01-17 18:21:54 -0300

    adjust structure project

--- src/api2/app.py
+++ src/api2/app.py
@@ -1,38 +1,22 @@
-from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify 
+import requests
+import subprocess
+import json
 import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
-
-@app.route("/", methods=["POST"])
+@app.route("/", methods=["GET","POST"])
 def create_event():
-    if request.method == 'POST':
-        content = request.get_json()
-        return jsonify(content) 
-        msg = ''
-    if request.method == 'POST': 
-        email       = request.form['email']
-        password    = request.form['password']
-        first_name  = request.form['first_name']
-        last_name   = request.form['last_name']
-        criacao     = datetime.now().strftime('%Y%m%d')
-        alteracao   = None
-    
-        cursor.execute('INSERT INTO usuario (email, password, first_name, last_name, criacao, alteracao) VALUES (?,?,?,?,?,?)', (email,password,first_name,last_name,criacao,alteracao))
-        cursor.commit()
-        msg = 'Registro efetuado com sucesso!'
-        return redirect(url_for('registrar'))
-    else:
-        None
+    with open('backend.json', 'a') as f:
+        json_ = json.loads(request.data)
+        json.dump(json_, f)
+    return json_
 
-@app.route("/test", methods=["GET"])
+@app.route("/test", methods=["GET","POST"])
 def open_event():
     response = requests.get('http://localhost:5000').text
-    # data = json.loads(response)
-    data = response
+    data = json.loads(response)
     return data
 
 if __name__ == "__main__":
