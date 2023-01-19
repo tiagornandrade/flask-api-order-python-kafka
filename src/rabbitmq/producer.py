db2d461f4b97520f995eaff297f2c8d66e7d193d
Tiago Navarro <tiagornandrade@hotmail.com>
2023-01-19 10:41:54 -0300

    add implementation of kafka in api

--- src/rabbitmq/producer.py
+++ src/rabbitmq/producer.py
@@ -2,23 +2,23 @@ import pika
 import time
 import random
 
-connection_parameters = pika.ConnectionParameters("localhost")
+connection_parameters = pika.ConnectionParameters('localhost')
 
 connection = pika.BlockingConnection(connection_parameters)
 
 channel = connection.channel()
 
-channel.queue_declare(queue="letterbox")
+channel.queue_declare(queue='letterbox')
 
 messageId = 1
 
-while True:
+while(True):
     message = f"Sending Message Id: {messageId}"
 
-    channel.basic_publish(exchange="", routing_key="letterbox", body=message)
+    channel.basic_publish(exchange='', routing_key='letterbox', body=message)
 
     print(f"sent message: {message}")
-
+    
     time.sleep(random.randint(1, 4))
 
-    messageId += 1
+    messageId+=1
