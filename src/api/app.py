0b605b3137c624cc89b31b3827735b2f7cd66f9a
Tiago Navarro <tiagornandrade@hotmail.com>
2023-02-01 17:21:21 -0300

    refact path structure

--- src/api/app.py
+++ src/api/app.py
@@ -1,23 +1,31 @@
 from flask import Flask, request, jsonify
+from tinydb import TinyDB, Query
 import requests
 import json
 
 
 app = Flask(__name__)
 
+db = TinyDB('backend.json')
 
 @app.route("/", methods=["GET", "POST"])
 def create_event():
     if request.method == "POST":
-        content = request.get_json()
+        content = db.insert(request.get_json())
         return jsonify(content)
 
 
 @app.route("/test", methods=["GET", "POST"])
 def open_event():
-    response = requests.get("http://localhost:5000").text
-    data = json.loads(response)
-    return data
+    #response = requests.get("http://localhost:5000").text
+    #data = json.loads(response)
+    #return data
+    return json.load(db.all())
+
+    #with open('backend.json') as f:
+    #    feeds = json.load(f)
+    #    n = [a for a in feeds]
+    #    return jsonify(n)
 
 
 if __name__ == "__main__":
