7eee5c7fa0788fa2279b45627cbb4e07f4fa26a2
Tiago Navarro <tiagornandrade@hotmail.com>
2023-02-25 14:23:03 -0300

    feat: creating shell scripts

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 open ./shell/app.sh;
 open ./shell/appOrder.sh;
-open ./shell/Transaction.sh;
+open ./shell/appTransaction.sh;
 open ./shell/consumer_order_created.sh;
 open ./shell/consumer_order_deleted.sh;
 open ./shell/consumer_transaction.sh;
