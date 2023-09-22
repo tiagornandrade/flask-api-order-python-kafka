2eb4b033dc2a627dc2e04318e6b6f655ed98fe94
tiagornandrade <tiagornandrade@hotmail.com>
2023-09-22 14:07:31 -0300

    refact: refactoring routes file

--- py-flink/consumer.py
+++ py-flink/consumer.py
@@ -7,7 +7,6 @@ from pyflink.table.window import Tumble
 
 class OrderConsumerFunction(ScalarFunction):
     def eval(self, value):
-        # Process the consumed message here
         print(value)
 
 
@@ -15,14 +14,12 @@ def create_flink_consumer():
     env = StreamExecutionEnvironment.get_execution_environment()
     t_env = StreamTableEnvironment.create(env)
 
-    # Set up Kafka connector properties
     kafka_props = {
         'bootstrap.servers': 'localhost:9092',
         'group.id': 'flink_consumer_group',
         'auto.offset.reset': 'earliest'
     }
 
-    # Define the schema for the JSON messages
     schema = Schema()
     schema.field('user_id', 'STRING')
     schema.field('event_key', 'STRING')
@@ -31,7 +28,6 @@ def create_flink_consumer():
     schema.field('price', 'DOUBLE')
     schema.field('operation', 'STRING')
 
-    # Register the Kafka connector
     t_env.connect(Kafka()
                   .version("universal")
                   .topic('order_created')
@@ -42,12 +38,10 @@ def create_flink_consumer():
                   .json_schema(schema)
                   .register_schema_as_table('orders'))
 
-    # Define the query to consume the messages and apply the OrderConsumerFunction
     t_env.from_path('orders') \
         .select("*") \
         .to_pandas().apply(lambda row: OrderConsumerFunction().eval(row), axis=1)
 
-    # Execute the Flink job
     env.execute("Flink Kafka Consumer")
 
 
--- src/views/routes.py
+++ src/views/routes.py
@@ -1,39 +1,49 @@
 from flask import Flask, request, jsonify
 from src.model.order_model import orderGetItem
 from src.model.transaction_model import transactionGetItem
-from src.plugins.producer import *
+from confluent_kafka import Producer
+from datetime import datetime
+
+app = Flask(__name__)
+
+conf = {
+    'bootstrap.servers': 'localhost:9092',
+}
+
+producer = Producer(conf)
 
 
 def init_app(app: Flask):
-    @app.route("/order/create_item", methods=["POST"])
-    def create_item():
-        if request.method == "POST":
-            data = request.get_json()
-            produce_message("created", data)
-        return jsonify({'message': 'Dado inserido com sucesso!'})
+    @app.route('/create', methods=['POST'])
+    def create():
+        data = request.json
+        producer.produce('my_topic', key='create', value=str({'payload': [data], 'is_deleted': False, 'created_at': datetime.now()}))
+        producer.flush()
+        return jsonify({'message': 'Created successfully'}), 201
 
 
-    @app.route("/order/update_item", methods=["PUT"])
-    def update_item():
-        if request.method == "PUT":
-            data = request.get_json()
-            produce_message("updated", data)
-        return jsonify({'message': 'Dado atualizado com sucesso!'})
+    @app.route('/update/<int:id>', methods=['PUT'])
+    def update(id):
+        data = request.json
+        producer.produce('my_topic', key='update', value=str({'id': id, 'data': data, 'is_deleted': False}))
+        producer.flush()
+        return jsonify({'message': f'Updated {id} successfully'}), 200
 
 
-    @app.route("/order/delete_item", methods=["DELETE"])
-    def delete_item():
-        if request.method == "DELETE":
-            data = request.get_json()
-            produce_message("deleted", data)
-        return jsonify({'message': 'Dado excluido com sucesso!'})
+    @app.route('/delete/<int:id>', methods=['DELETE'])
+    def delete(id):
+        producer.produce('my_topic', key='delete', value=str({'id': id, 'is_deleted': True}))
+        producer.flush()
+        return jsonify({'message': f'Deleted {id} successfully'}), 200
 
 
-    @app.route("/order/get_item", methods=["GET"])
-    def order_get_item():
-        return orderGetItem()
+    @app.route('/list', methods=['GET'])
+    def list_items():
+        items = [{'id': 1, 'name': 'Item 1'}, {'id': 2, 'name': 'Item 2'}]
+        producer.produce('my_topic', key='list', value=str(items))
+        producer.flush()
+        return jsonify(items), 200
 
 
-    @app.route("/transaction/get_item", methods=["GET"])
-    def transaction_get_item():
-        return transactionGetItem()
+if __name__ == '__main__':
+    app.run(debug=True)
