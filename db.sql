fa21032bd354fa8a0cf2c6df0306c0dc77225e27
Tiago Navarro <tiagornandrade@hotmail.com>
2024-07-02 11:10:57 -0300

    refact: Refatora projeto

--- db.sql
+++ db.sql
@@ -1,6 +1,8 @@
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO repl_user;
 ALTER USER repl_user WITH SUPERUSER;
 
+CREATE SCHEMA raw;
+
 CREATE TABLE public.orders (
     user_id SERIAL PRIMARY KEY,
     order_id UUID NOT NULL UNIQUE,
@@ -15,4 +17,15 @@ CREATE TABLE public.orders (
 CREATE TABLE public.transactions (
     transaction_id VARCHAR PRIMARY KEY,
     transaction JSONB
+);
+
+CREATE TABLE raw.orders (
+    user_id SERIAL PRIMARY KEY,
+    order_id UUID NOT NULL UNIQUE,
+    event_key VARCHAR,
+    product_name VARCHAR,
+    description VARCHAR,
+    price DOUBLE PRECISION,
+    event_timestamp TIMESTAMP,
+    operation VARCHAR
 );
